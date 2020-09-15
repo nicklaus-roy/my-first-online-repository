b7fd66cbca3146e5411facedd3684d312c16e15e
Sample <sampleemailonly854@gmail.com>
2020-09-14 17:16:26 -0700

    My Change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Welcome to my site
+                    This is my change
                 </div>
 
                 <div class="links">
